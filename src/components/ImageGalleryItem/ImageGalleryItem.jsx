a1db8de017d09d636b18a19e3d310f35a94d85a6
tiramisa <miller19988@gmail.com>
2023-08-29 12:33:50 +0100

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,57 +1,51 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Modal from 'react-modal';
 import styles from '../../css/styles.module.css';
 
 Modal.setAppElement('#root');
 
-class ImageGalleryItem extends Component {
-  state = {
-    isModalOpen: false,
+const ImageGalleryItem = ({ item }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
   };
 
-  openModal = () => {
-    this.setState({ isModalOpen: true });
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
 
-  closeModal = () => {
-    this.setState({ isModalOpen: false });
-  };
+  const { id, webformatURL, largeImageURL, tags } = item;
 
-  render() {
-    const { id, webformatURL, largeImageURL, tags } = this.props.item;
-    const { isModalOpen } = this.state;
+  return (
+    <li key={id} className={styles.ImageGalleryItem}>
+      <img
+        className={styles.ImageGalleryItemImage}
+        src={webformatURL}
+        alt={tags}
+        onClick={openModal}
+      />
 
-    return (
-      <li key={id} className={styles.ImageGalleryItem}>
-        <img
-          className={styles.ImageGalleryItemImage}
-          src={webformatURL}
-          alt={tags}
-          onClick={this.openModal}
-        />
-
-        <Modal
-          className={styles.Modal}
-          isOpen={isModalOpen}
-          onRequestClose={this.closeModal}
-          contentLabel="Image Modal"
-        >
-          <img src={largeImageURL} alt={tags} />
-          <button className={styles.ModalButton} onClick={this.closeModal}>
-            x
-          </button>
-        </Modal>
-      </li>
-    );
-  }
-}
+      <Modal
+        className={styles.Modal}
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        contentLabel="Image Modal"
+      >
+        <img src={largeImageURL} alt={tags} />
+        <button className={styles.ModalButton} onClick={closeModal}>
+          x
+        </button>
+      </Modal>
+    </li>
+  );
+};
 
 ImageGalleryItem.propTypes = {
   item: PropTypes.shape({
     id: PropTypes.number.isRequired,
     previewURL: PropTypes.string.isRequired,
-
     tags: PropTypes.string.isRequired,
   }).isRequired,
 };
